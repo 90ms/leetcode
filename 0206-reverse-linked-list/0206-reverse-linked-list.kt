7f522086d762c1f460bb438ef7fe7931cc368998
MyeongSung Kim <33411792+90ms@users.noreply.github.com>
2022-10-25 23:34:41 +0900

    Time: 342 ms (10.41%), Space: 35.9 MB (85.00%) - LeetHub

---
+++ 0206-reverse-linked-list/0206-reverse-linked-list.kt
@@ -0,0 +1,19 @@
+/**
+ * Example:
+ * var li = ListNode(5)
+ * var v = li.`val`
+ * Definition for singly-linked list.
+ * class ListNode(var `val`: Int) {
+ *     var next: ListNode? = null
+ * }
+ */
+class Solution {
+    fun reverseList(head: ListNode?): ListNode? {
+        return head?.next?.let{
+            reverseList(it).apply{
+                head.next.next = head
+                head.next = null
+            }
+        }?: head
+    }
+}
